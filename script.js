28d00e22569ad19ddaded453cb9804e59113115d
Naseef03 <muhammednaseef03@gmail.com>
2022-06-27 10:23:03 +0530

    Add functionality to display
    Now display will be populated when buttons are clicked except =, clear.

--- script.js
+++ script.js
@@ -11,9 +11,32 @@ function operate(operator, a, b) {
     return add(a, b);
   } else if (operator == '-') {
     return subtract(a, b);
-  } else if (operator == '*') {
+  } else if (operator == 'x') {
     return multiply(a, b);
   } else if (operator == '/') {
-    return divide(a, b)
+    return divide(a, b);
   }
 }
+
+
+function addNum(e) {
+  const num = e.target.textContent;
+  displayDiv.textContent += num;
+}
+
+function addoperator(e) {
+  const operator = e.target.textContent;
+  displayDiv.textContent += ' ' + operator + ' ';
+}
+
+const displayDiv = document.querySelector('#display');
+const numButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+
+numButtons.forEach(button => {
+  button.addEventListener('click', addNum);
+});
+
+operatorButtons.forEach(button => {
+  button.addEventListener('click', addoperator);
+});
